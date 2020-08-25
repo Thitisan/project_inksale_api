3a8d47f86bed5112a54d8c19c8d993e4b1d09dc9
Thitisan <thitisan.th@gmail.com>
2020-08-26 03:04:38 +0700

    test

---
+++ app/Http/Controllers/Api/CustomerController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Customer;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\CustomerResource;
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $customers = Customer::all();
+
+        return new CustomerResource($customers);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $customer = new Customer();
+        $customer->name=$request->name;
+        $customer->address=$request->address;
+        $customer->phone=$request->phone;
+
+        if($customer->save()){
+            return['status'=>'data has been inserted'];
+        }
+    }
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Customer $customer)
+    {
+        return response()->json($customer);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return Customer::destroy($id);
+    }
+}
